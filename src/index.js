42030cd8f4045d099fc7d59f9bed2eafb64b87b3
Arijit Bose <arijit.bose@indusnet.co.in>
2023-12-22 11:52:53 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
-import connectDB from './db/index.js'
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB()  
+connectDB()
+  .then((res) => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`SERVER IS RUNNING AT PORT: ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED !!", err)
+  });
