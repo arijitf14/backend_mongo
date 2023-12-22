9bca9fd071f200ef0fcd0c90ef1c97097e37a7f4
Arijit Bose <arijit.bose@indusnet.co.in>
2023-12-22 13:01:54 +0530

    Controllers and Routes initiated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+app.use("/api/v1/users", userRouter)
 
 export { app }
